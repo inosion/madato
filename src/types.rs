b9094260096f245c9d24dc28c8e323eda2dfc9e8
Ramon Buckland <ramon@thebuckland.com>
2018-07-26 01:13:11 +1000

    generates YAML from XLSX

--- src/types.rs
+++ src/types.rs
@@ -5,3 +5,5 @@ pub type TableRow<K, V> = LinkedHashMap<K, V>;
 pub type Table<K, V> = Vec<TableRow<K, V>>;
 pub type MultiTables<K, V> = Vec<Table<K, V>>;
 pub type Headers = IndexSet<String>;
+pub type NamedTable<K, V> = (String, Table<K, V>);
+pub type ErroredTable     = (String, String);
